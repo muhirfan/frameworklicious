3b6b2634adea14a2d9f5324e0b390f7ad9e15d2e
“kauhik” <kaushikmanian456@gmail.com>
2025-04-24 16:22:25 +0800

    Initial Commit

---
+++ WidgetKit Exploration/WidgetKit_ExplorationApp.swift
@@ -0,0 +1,32 @@
+//
+//  WidgetKit_ExplorationApp.swift
+//  WidgetKit Exploration
+//
+//  Created by Kaushik Manian on 24/4/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct WidgetKit_ExplorationApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
