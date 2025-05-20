0636c6a6cd7ae2955e2331b22b4c2c0f0467a464
“kauhik” <kaushikmanian456@gmail.com>
2025-05-20 08:29:44 +0800

    Initial Commit

---
+++ Arkit DemoTests/Arkit_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Arkit_DemoTests.swift
+//  Arkit DemoTests
+//
+//  Created by Kaushik Manian on 20/5/25.
+//
+
+import Testing
+@testable import Arkit_Demo
+
+struct Arkit_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Arkit DemoUITests/Arkit_DemoUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Arkit_DemoUITestsLaunchTests.swift
+//  Arkit DemoUITests
+//
+//  Created by Kaushik Manian on 20/5/25.
+//
+
+import XCTest
+
+final class Arkit_DemoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
