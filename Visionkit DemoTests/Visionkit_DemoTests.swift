a008feba6b333ff227f0bedd01b49ff114c23b08
“kauhik” <kaushikmanian456@gmail.com>
2025-05-20 10:20:46 +0800

    Initial Commit

---
+++ Visionkit DemoTests/Visionkit_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Visionkit_DemoTests.swift
+//  Visionkit DemoTests
+//
+//  Created by Kaushik Manian on 20/5/25.
+//
+
+import Testing
+@testable import Visionkit_Demo
+
+struct Visionkit_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
